410faa1f285dde325411fb02b5801698785185f2
Oktado Putra <oktado.putra@gmail.com>
2020-10-23 08:54:16 +0700

    first

---
+++ script.js
@@ -0,0 +1,6 @@
+let peserta = ['Oktado','Naufal','Oji','Raka','Dheal'];
+ let teamSatu = peserta.slice(0,2);
+ let teamDua = peserta.slice(2)
+ 
+console.log(teamSatu.join());
+console.log(teamDua.join());
